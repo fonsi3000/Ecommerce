dd6ac6b2b1f064ca1fbda31040ae56c2a3034346
fonsi3000 <luiiscarrascal@gmail.com>
2025-05-14 07:51:18 -0500

    Deploy

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 echo "⏳ Esperando que MySQL esté disponible..."
-until nc -z registros_db 3306; do
+until nc -z expo_db 3306; do
   echo "MySQL aún no responde, reintentando..."
   sleep 2
 done
 echo "✅ MySQL disponible"
 
 echo "⏳ Esperando que Redis esté disponible..."
-until nc -z registros_redis 6379; do
+until nc -z expo_redis 6379; do
   echo "Redis aún no responde, reintentando..."
   sleep 2
 done
